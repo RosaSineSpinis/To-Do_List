786d1719ef615d81b65cd108ee584dbb85f94d9a
Piotr <piotr.w.miecz@gmail.com>
2021-05-26 18:25:54 +0200

    working sql alchemy project

--- PycharmProjects/HyperTube/.idea/VirtualEnvironment/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\piotr\PycharmProjects\HyperTube\.idea\VirtualEnvironment\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- PycharmProjects/HyperTube/HyperTube/task/hypertube/tube/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Tag, Video, VideoTag
-
-admin.site.register(Tag)
-admin.site.register(Video)
-admin.site.register(VideoTag)
